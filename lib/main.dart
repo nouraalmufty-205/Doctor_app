5b5d17dfaaa50abfe5a759b067439d9da7b0e9b1
Inagdy <islam.m.nagdy@gmail.com>
2026-09-13 04:27:18 +0200

    adding medicineorder screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:doctorapp/screens/doctorselecttime1.dart';
 import 'package:doctorapp/screens/doctorselecttime2.dart';
 import 'package:doctorapp/screens/finddoctorscreen.dart';
 import 'package:doctorapp/screens/homescreen.dart';
+import 'package:doctorapp/screens/medicine_order.dart/medicine_order_screen.dart';
 import 'package:doctorapp/screens/menu_screen/menu_screen.dart';
 import 'package:doctorapp/screens/livescreen.dart';
 import 'package:doctorapp/screens/my_doctors/my_doctors_screen.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Doctorselecttime2(),
+      home: MedicineOrderScreen(),
     );
   }
 }
